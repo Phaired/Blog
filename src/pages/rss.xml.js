ebdb555e20ba4c1fb7fdd37d5eee4c8645418802
Phaired <65019388+Phaired@users.noreply.github.com>
2025-06-04 11:51:24 +0200

    Add multilingual routing and translations (#144)

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,19 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
+import { DEFAULT_LANG } from "../i18n/utils";
 
 export async function GET(context) {
-    const posts = await getCollection("blog");
+    const posts = (await getCollection("blog")).filter(
+        (p) => p.data.lang === DEFAULT_LANG,
+    );
     return rss({
         title: SITE_TITLE,
         description: SITE_DESCRIPTION,
         site: context.site,
         items: posts.map((post) => ({
             ...post.data,
-            link: `/blog/${post.slug}/`,
+            link: `/${DEFAULT_LANG}/blog/${post.slug}/`,
         })),
     });
 }
